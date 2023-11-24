ab8659d7611ff4c024c7fa693e932fa98f6ac529
brunuhamaduh <a2019132612@isec.pt>
2023-11-24 11:51:08 +0000

    add bat files (windows)

---
+++ batch/compile.bat
@@ -0,0 +1,4 @@
+@echo off
+javac -d ../out ../src/pt/isec/brago/eventsManager/*.java
+echo "Compiled successfully"
+pause
--- batch/compileWIN.bat
+++
@@ -1,4 +0,0 @@
-javac -d ..\bin\..\src\*.java
-
-pause
-echo "Events manager compiled. Press Enter to continue..."
